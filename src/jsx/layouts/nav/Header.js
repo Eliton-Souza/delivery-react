bc230a7c1c5137eea21b0147efd033c2b7153b7c
ElitonSouza <ess3@icomp.ufam.edu.br>
2024-02-06 08:04:55 -0400

    personalizando header

--- src/jsx/layouts/nav/Header.js
+++ src/jsx/layouts/nav/Header.js
@@ -9,14 +9,14 @@ import { Link } from "react-router-dom";
 //import profile from "../../../images/profile/pic1.jpg";
 //import avatar from "../../../images/avatar/1.jpg";
 //import { Dropdown } from "react-bootstrap";
-import LogoutPage from './Logout';
+
 
 import HeaderSlider from './HeaderSlider';
 
 
 //import avatar from "../../../images/avatar/1.jpg";
-import profile from "../../../images/banner-img/pic-1.png";
-import { dadosUsuario, getToken } from "../../../services/api";
+import semImagem from "../../../images/no-img-avatar.png";
+import { dadosUsuario, getToken, removeToken } from "../../../services/api";
 
 const LocationIcon = <i className="fa-solid fa-location-dot mx-2 " />;
 
@@ -56,7 +56,13 @@ const Header = ({ onNote }) => {
 	
 		// Chama a função assíncrona
 		fetchUsuario();
-	  }, [getToken()]); 
+	  }, [getToken()]);
+
+	  const logout = () => {
+		removeToken();
+		window.location.href = '/dashboard';
+	  }
+	  
 
 
 
@@ -80,31 +86,6 @@ const Header = ({ onNote }) => {
 		});
 	}, []); 
 	
-  //const [searchBut, setSearchBut] = useState(false);	
-  //var path = window.location.pathname.split("/");
-  //var name = path[path.length - 1].split("-");
-  //var filterName = name.length >= 3 ? name.filter((n, i) => i > 0) : name;
-  //var finalName = filterName.includes("app")
-  //  ? filterName.filter((f) => f !== "app")
-  //  : filterName.includes("ui")
-  //  ? filterName.filter((f) => f !== "ui")
-  //  : filterName.includes("uc")
-  //  ? filterName.filter((f) => f !== "uc")
-  //  : filterName.includes("basic")
-  //  ? filterName.filter((f) => f !== "basic")
-  //  : filterName.includes("table")
-  //  ? filterName.filter((f) => f !== "table")
-  //  : filterName.includes("page")
-  //  ? filterName.filter((f) => f !== "page")
-  //  : filterName.includes("email")
-  //  ? filterName.filter((f) => f !== "email")
-  //  : filterName.includes("ecom")
-  //  ? filterName.filter((f) => f !== "ecom")
-  //  : filterName.includes("chart")
-  //  ? filterName.filter((f) => f !== "chart")
-  //  : filterName.includes("editor")
-  //  ? filterName.filter((f) => f !== "editor")
-  //  : filterName;
   return ( 
     <div className={`header ${headerFix ? "is-fixed" : ""}`}>
       <div className="header-content">
@@ -180,25 +161,25 @@ const Header = ({ onNote }) => {
 									//onClick={DropBtnblog()}
 								>
 									<div className="header-info2 d-flex align-items-center">
-										<img src={profile} alt="" />
+										<img src={avatar??semImagem} alt="" />
 										<div className="d-flex align-items-center sidebar-info">
 											<div>
-												<h6 className="font-w500 mb-0 ms-2">{nome}</h6>
+												<h6 className="font-w500 mb-0 ms-2">{nome??"Conta"}</h6>
 											</div>	
 											<i className="fas fa-chevron-down"></i>
 										</div>
 										
 									</div>
 								</Dropdown.Toggle>
+
+								{nome && (
 								<Dropdown.Menu className="dropdown-menu-end">
+
+								
 									<Link to="./app-profile" className="dropdown-item ai-icon ">
 										<svg  xmlns="http://www.w3.org/2000/svg" className="text-primary" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
 										<span className="ms-2">Perfil</span>
 									</Link>									
-									<Link to="./edit-profile" className="dropdown-item ai-icon">
-										<svg xmlns="http://www.w3.org/2000/svg" className="text-primary" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
-										<span className="ms-2">Editar Perfil</span>
-									</Link>
 									<Link to="./email-inbox" className="dropdown-item ai-icon">
 										<svg  xmlns="http://www.w3.org/2000/svg" className="text-primary" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
 										<span className="ms-2">Pedidos</span>
@@ -232,8 +213,36 @@ const Header = ({ onNote }) => {
 										</svg>
 										<span className="ms-2">Settings </span>
 									</Link>
-									{/*<LogoutPage /> corrigir pagina de logout		*/}
+									<Link to="#" className="dropdown-item ai-icon" onClick={logout}>
+										<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" className="svg-main-icon">
+											<g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+												<rect x="0" y="0" width="24" height="24"/>
+												<path d="M18.6225,9.75 L18.75,9.75 C19.9926407,9.75 21,10.7573593 21,12 C21,13.2426407 19.9926407,14.25 18.75,14.25 L18.6854912,14.249994 C18.4911876,14.250769 18.3158978,14.366855 18.2393549,14.5454486 C18.1556809,14.7351461 18.1942911,14.948087 18.3278301,15.0846699 L18.372535,15.129375 C18.7950334,15.5514036 19.03243,16.1240792 19.03243,16.72125 C19.03243,17.3184208 18.7950334,17.8910964 18.373125,18.312535 C17.9510964,18.7350334 17.3784208,18.97243 16.78125,18.97243 C16.1840792,18.97243 15.6114036,18.7350334 15.1896699,18.3128301 L15.1505513,18.2736469 C15.008087,18.1342911 14.7951461,18.0956809 14.6054486,18.1793549 C14.426855,18.2558978 14.310769,18.4311876 14.31,18.6225 L14.31,18.75 C14.31,19.9926407 13.3026407,21 12.06,21 C10.8173593,21 9.81,19.9926407 9.81,18.75 C9.80552409,18.4999185 9.67898539,18.3229986 9.44717599,18.2361469 C9.26485393,18.1556809 9.05191298,18.1942911 8.91533009,18.3278301 L8.870625,18.372535 C8.44859642,18.7950334 7.87592081,19.03243 7.27875,19.03243 C6.68157919,19.03243 6.10890358,18.7950334 5.68746499,18.373125 C5.26496665,17.9510964 5.02757002,17.3784208 5.02757002,16.78125 C5.02757002,16.1840792 5.26496665,15.6114036 5.68716991,15.1896699 L5.72635306,15.1505513 C5.86570889,15.008087 5.90431906,14.7951461 5.82064513,14.6054486 C5.74410223,14.426855 5.56881236,14.310769 5.3775,14.31 L5.25,14.31 C4.00735931,14.31 3,13.3026407 3,12.06 C3,10.8173593 4.00735931,9.81 5.25,9.81 C5.50008154,9.80552409 5.67700139,9.67898539 5.76385306,9.44717599 C5.84431906,9.26485393 5.80570889,9.05191298 5.67216991,8.91533009 L5.62746499,8.870625 C5.20496665,8.44859642 4.96757002,7.87592081 4.96757002,7.27875 C4.96757002,6.68157919 5.20496665,6.10890358 5.626875,5.68746499 C6.04890358,5.26496665 6.62157919,5.02757002 7.21875,5.02757002 C7.81592081,5.02757002 8.38859642,5.26496665 8.81033009,5.68716991 L8.84944872,5.72635306 C8.99191298,5.86570889 9.20485393,5.90431906 9.38717599,5.82385306 L9.49484664,5.80114977 C9.65041313,5.71688974 9.7492905,5.55401473 9.75,5.3775 L9.75,5.25 C9.75,4.00735931 10.7573593,3 12,3 C13.2426407,3 14.25,4.00735931 14.25,5.25 L14.249994,5.31450877 C14.250769,5.50881236 14.366855,5.68410223 14.552824,5.76385306 C14.7351461,5.84431906 14.948087,5.80570889 15.0846699,5.67216991 L15.129375,5.62746499 C15.5514036,5.20496665 16.1240792,4.96757002 16.72125,4.96757002 C17.3184208,4.96757002 17.8910964,5.20496665 18.312535,5.626875 C18.7350334,6.04890358 18.97243,6.62157919 18.97243,7.21875 C18.97243,7.81592081 18.7350334,8.38859642 18.3128301,8.81033009 L18.2736469,8.84944872 C18.1342911,8.99191298 18.0956809,9.20485393 18.1761469,9.38717599 L18.1988502,9.49484664 C18.2831103,9.65041313 18.4459853,9.7492905 18.6225,9.75 Z" fill="var(--primary)" fillRule="nonzero" opacity="0.3"/>
+												<path d="M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z" fill="var(--primary)"/>
+											</g>
+										</svg>
+										<span className="ms-2">Sair</span>
+									</Link>
 								</Dropdown.Menu>
+								)}
+
+
+								{!nome && (
+								<Dropdown.Menu className="dropdown-menu-end">
+									<Link to="./login" className="dropdown-item ai-icon ">
+										<svg  xmlns="http://www.w3.org/2000/svg" className="text-primary" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+										<span className="ms-2">Login</span>
+									</Link>									
+									<Link to="./cadastro-usuario" className="dropdown-item ai-icon">
+										<svg xmlns="http://www.w3.org/2000/svg" className="text-primary" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+										<span className="ms-2">Cadastrar</span>
+									</Link>
+								</Dropdown.Menu>
+								)}
+
+
+
+
 							</Dropdown>
 						</li>
 					</ul>
