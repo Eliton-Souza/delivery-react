540a7fb42d34e948d1f5d4081571108361eefe51
Eliton Souza <ess3@icomp.ufam.edu.br>
2024-06-04 19:49:28 -0400

    criando componente de preco para produto

---
+++ src/jsx/components/componentes/preco.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { Formik, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const Preco = ({ setPreco, placeholder, changeErro, desabilitado }) => {
+  
+  const [erro, setErro] = useState(false);
+
+  const validaPreco = Yup.object().shape({
+    texto: Yup.string()
+      .matches(/^\d+(\,\d+)?$/, 'Deve ser um valor numérico positivo separo por vírgula')
+      .required('O campo Preço é obrigatório')
+  });  
+    
+  return (
+    <div className="row">
+      <div className="col-12">
+        <Formik
+          initialValues={{ texto: "" }}
+          validationSchema={validaPreco}
+        >
+          {({ values, errors, handleChange, handleBlur }) => {
+
+            useEffect(() => {
+              if (errors.texto) {
+                changeErro(true);
+                setErro(true);
+              } else {
+                changeErro(false);
+                setErro(false);
+              }
+            }, [errors.texto]);
+
+            return (
+              <form>
+                <div className={`form-group mb-3 ${values.texto ? errors.texto ? "is-invalid"  : "is-valid" : ""}`}>
+                  <div className={`input-group mb-3 input-${erro ? 'danger' : 'warning'}`}>
+                                
+                    <Field
+                      type="text"
+                      name="texto"
+                      className="form-control input-rounded"
+                      disabled={desabilitado}
+                      placeholder={placeholder}
+                      onChange={(event) => {
+                        handleChange(event);
+                        setPreco(event.target.value);
+                      }}
+                      onBlur={handleBlur}                      
+                    />
+                   
+                    <ErrorMessage
+                      name="texto"
+                      component="div"
+                      className="invalid-feedback animated fadeInUp"
+                      style={{ display: "block" }}
+                    />
+                  </div>
+                </div>
+              </form>
+            );
+          }}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default Preco;
