e3755a5e402c0f8e9ea836354c01454890019d71
Eliton Souza <ess3@icomp.ufam.edu.br>
2024-06-05 13:50:32 -0400

    simplificando a tela de seleção de tipo e adicionando icone de produto

--- src/jsx/views/Funcionario/Loja/Produtos/cadastrarProduto.js
+++ src/jsx/views/Funcionario/Loja/Produtos/cadastrarProduto.js
@@ -1,11 +1,10 @@
-import React, { Fragment, useState } from "react";
-import imagem from "../../../../../images/no-img-avatar.png"
-import { Button, Card, CardBody, CardHeader } from "react-bootstrap";
+import React, { Fragment, useEffect, useState } from "react";
+import imagem from "../../../../../images/pacote.png"
+import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import TituloPagina from "../../../../components/componentes/TituloPagina";
-import SelecaoTipoProduto from "./selecaoTipoProduto";
-
-
+import CadastrarProdutoSimples from "./cadastrarProdutoSimples";
+import { OpcoesHamburgueriaLanchonetePizzaria, OpcoesRestaurante } from "./helper";
 
 const CadastrarProduto = () => {
 
@@ -13,45 +12,70 @@ const CadastrarProduto = () => {
 
 	const [loading, setLoading]= useState(false);
 
+   const [tipo, setTipo] = useState(null);
    const [step, setStep]= useState(1);
 
+   useEffect(() => {
+      if(step==1){
+         setTipo(null);
+      }
+    }, [step]); 
 	
 	return (
 		<Fragment>
          <TituloPagina titulo={"/Cadastrar Produto"}></TituloPagina>
          <br></br>
-
+         
          {step == 1 && (   
             <div>
                <div className="mb-3 d-flex justify-content-center align-items-center">
-                  <h6>Seu produto foi preparado por você ou pela indústria?</h6>
+                  <h4>Selecione o tipo do seu produto</h4>
                </div>
-               <div className="row">
-                  <div className="col-6">
-                     <SelecaoTipoProduto step={2} setStep={setStep} imagem={imagem} tipo={"Preparado"} descricao={"Pizzas, hamburguers, bolos..."}></SelecaoTipoProduto>
-                  </div>
-                  <div className="col-6">
-                     <SelecaoTipoProduto step={4} setStep={setStep} imagem={imagem} tipo={"Industrializado"} descricao={"refrigerantes, chocolates, sorvetes..."}></SelecaoTipoProduto>
+               <div className="row d-flex justify-content-center align-items-center">
+                  <div className="col-12 col-sm-6">
+                     <div className="card overflow-hidden">
+                        <div className="card-body">
+                           <div className="text-center">
+                              <div className="profile-photo">
+                                 <img
+                                    src={imagem}
+                                    width="100"
+                                    className="img-fluid rounded-circle"
+                                    alt=""
+                                 />
+                              </div>
+
+                              <h5 className="mt-4 mb-1">Tipo de Produto</h5>
+
+                              <div className="form-group mb-3">
+                                 <select defaultValue="" className="form-control" onChange={(e) => setTipo(e.target.value)}>
+                                    <option value="" disabled>
+                                       Selecione uma opção
+                                    </option>
+                                    <OpcoesHamburgueriaLanchonetePizzaria></OpcoesHamburgueriaLanchonetePizzaria>
+                                 </select>
+                              </div>
+
+                              <Button disabled={!tipo} onClick={()=> tipo=='pizza' ? setStep(4) : setStep(2)} className="me-2" variant="outline-primary btn-rounded">
+                                 Continuar
+                              </Button>
+                           </div>
+                        </div>
+                     </div>
                   </div>
                </div>
             </div>
          )}
-         
+
          {step == 2 && (   
-            <div>
-               <div className="mb-3 d-flex justify-content-center align-items-center">
-                  <h6>Seu produto é uma Pizza ou de outro tipo?</h6>
-               </div>
-               <div className="row">
-                  <div className="col-6">
-                     <SelecaoTipoProduto step={3} setStep={setStep} imagem={imagem} tipo={"Pizza"} descricao={"Pizza Média, Pizza Família..."}></SelecaoTipoProduto>
-                  </div>
-                  <div className="col-6">
-                     <SelecaoTipoProduto step={4} setStep={setStep} imagem={imagem} tipo={"Outro"} descricao={"hamburguers, bolos, sobremesas..."}></SelecaoTipoProduto>
-                  </div>
-               </div>
-            </div>
+            <CadastrarProdutoSimples tipo={tipo} setStep={setStep}></CadastrarProdutoSimples>
          )}
+
+
+         
+
+
+
 		</Fragment>
 	);
 };
